e41fc582dae3235aaaea479ff835769f5dfc6177
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-12-18 13:17:36 +0530

    Track ShareMe_Backend; add standard ignores

---
+++ ShareMe_Backend/schemaTypes/index.js
@@ -0,0 +1,15 @@
+import  user  from "./User.js"
+import commentSchema from "./comment"
+import  saveSchema  from "./save"
+import  postedBySchema  from "./postedBy"
+import pin from "./pin"
+
+
+
+export const schemaTypes = [
+    user,
+    commentSchema,
+    saveSchema,
+    postedBySchema,
+    pin
+]
---
+++ ShareMe_Backend/schemaTypes/pin.js
@@ -0,0 +1,58 @@
+export default{
+    name : 'pin',
+    title : 'Pin',
+    type : 'document',
+    fields : [
+        {
+            name : 'title',
+            title : 'Title',
+            type : 'string',
+        },
+        {
+            name : 'about',
+            title : 'About',
+            type : 'string',
+        },
+        {
+            name : 'destination',
+            title : 'Destination',
+            type : 'url',
+        },
+        {
+            name : 'category',
+            title : 'Category',
+            type : 'string',
+           
+        },
+        {
+            name : 'image',
+            title : 'Image',
+            type : 'image',
+            options : {
+                hotspot : true    
+            }
+        },
+        {
+            name : 'postedBy',
+            title : 'Posted By',
+            type : 'postedBy',
+        },
+        {
+            name : 'userId',
+            title : 'UserID',
+            type : 'string',
+        },
+        {
+            name : 'save',
+            title : 'Save',
+            type : 'array',
+            of : [{type : 'save'}]
+        },
+        {
+            name : 'comments',
+            title : 'Comments',
+            type : 'array',
+            of : [{type : 'comment'}]
+        }
+    ]
+}
