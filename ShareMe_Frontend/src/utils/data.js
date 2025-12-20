e74fdc88dd73d1190ac8b2e4879fcf7fe6c61872
gauravhadge-29 <gauravhadge.official@gmail.com>
2025-12-20 18:46:20 +0530

    create pin page compeleted

--- ShareMe_Frontend/src/utils/data.js
+++ ShareMe_Frontend/src/utils/data.js
@@ -50,5 +50,50 @@ export const feedQuery = `*[_type == "pin"] | order(_createdAt desc){
             },
         },
 }`
+
+
+export const categories = [
+    {
+        name:'cars',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'fitness',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'wallpaper',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'websites',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'photo',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'food',    
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'nature',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'art',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+
+        name:'travel',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+    {
+        name:'quotes',
+        image:'https://i.pinimg.com/750x/eb/3d/1c/eb3d1c4676f2ed9640a1b4d8d2bcae18.jpg'
+    },
+]
     
 
