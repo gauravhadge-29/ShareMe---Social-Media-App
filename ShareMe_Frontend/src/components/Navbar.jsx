d4994c3d36eed9de56621bef32e6d2feec597018
gauravhadge-29 <gauravhadge.official@gmail.com>
2026-01-12 23:42:07 +0530

    login issue

--- ShareMe_Frontend/src/components/Navbar.jsx
+++ ShareMe_Frontend/src/components/Navbar.jsx
@@ -13,6 +13,14 @@ const Navbar = ({searchTerm, setSearchTerm,user}) => {
       </div>
 
       <div className='flex gap-3 '>
+        {!user && (
+          <Link
+            to="/login"
+            className='bg-red-500 text-white font-bold px-5 py-2 text-base rounded-3xl hover:opacity-90 hover:shadow-md outline-none'
+          >
+            Login
+          </Link>
+        )}
         {user && (
           <Link to={`user-profile/${user?._id}`} className='hidden md:block'>
               <img src={user?.image} alt="user" className='w-14 h-12 rounded-lg'/>
